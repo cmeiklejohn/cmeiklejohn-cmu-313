b40345d20faa860d4791fd082945ee7f6535dc7f
Jess Meng <jessicalmeng@gmail.com>
2020-09-09 01:38:54 -0400

    Changes by Jessica

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,5 @@
 public static void main(String args[]) {
 	System.out.println("Hello World, Christopher has been at CMU forever.");
 	System.out.println("Hello from Sean");
+    System.out.println("Hello from Jessica");
 }
